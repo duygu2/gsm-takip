ebf734b7e43e4f4d1b21c92db45d7ccaa8884a52
duygu2 <duyguorhan017@gmail.com>
2022-06-01 21:48:43 +0300

    heroku added mqtt server

--- client.js
+++ client.js
@@ -1,6 +1,19 @@
-var mqtt = require('mqtt');
-var client = mqtt.connect('mqtt://127.0.0.1:1883');
+const mqtt = require('mqtt');
 
+if (!process.env.MOSQUITTO_SERVER) {
+  throw Error('You should first fill the .env-example file and rename it to .env');
+}
+
+// Connection to MQTT server
+const client = mqtt.connect(
+  // Note: you can replace 'mqtts' per 'wss' to connect to Mosquitto using websockets (you need to activate the option on Stackhero console and to configure "Security shield" to allow your IPs).
+  'mqtts://' + process.env.MOSQUITTO_SERVER,
+  {
+    username: process.env.MOSQUITTO_USERNAME,
+    password: process.env.MOSQUITTO_PASSWORD,
+    clean: true
+  }
+);
 
 client.subscribe('location');
 
@@ -8,7 +21,7 @@ client.on('connect', function() {
 	console.log('connected!');
 
 	client.subscribe('new-user',function(){
-        client.publish('new-user', 'Mehmet-' + Math.ceil(Math.random() * 10),{
+        client.publish('new-user', 'Duygu-' + Math.ceil(Math.random() * 10),{
             retain: true,
         });   
     });
